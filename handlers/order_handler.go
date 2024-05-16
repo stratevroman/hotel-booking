9a51920512142adb528afbf1831d553bc83d967f
roman.stratev <stratev.roman@skyeng.ru>
2024-05-16 09:45:16 +0400

    added type of errors and hide email

--- handlers/order_handler.go
+++ handlers/order_handler.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"applicationDesignTest/models"
 	"applicationDesignTest/services"
+	"applicationDesignTest/utils"
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
@@ -19,14 +21,22 @@ func (oh *OrderHandler) CreateOrder(w http.ResponseWriter, r *http.Request) {
 	var newOrder models.Order
 	err := json.NewDecoder(r.Body).Decode(&newOrder)
 	if err != nil {
+		utils.LogErrorf("Decoding error", err.Error())
 		http.Error(w, "Invalid input data", http.StatusBadRequest)
 		return
 	}
 
 	err = oh.orderService.CreateOrder(newOrder)
 	if err != nil {
-		/**	@todo перехватывать тип ошибки и отдавать соответсвующий код */
-		http.Error(w, "Failed to create order "+err.Error(), http.StatusInternalServerError)
+		switch {
+		case errors.Is(err, utils.ErrIncorrectDates):
+			http.Error(w, "Incorrect date range "+err.Error(), http.StatusBadRequest)
+		case errors.Is(err, services.ErrNotAvailableRoom):
+			http.Error(w, "Selected room is not available "+err.Error(), http.StatusConflict)
+		default:
+			http.Error(w, "Failed to create order "+err.Error(), http.StatusInternalServerError)
+		}
+
 		return
 	}
 
--- main.go
+++ main.go
@@ -1,12 +1,3 @@
-// Ниже реализован сервис бронирования номеров в отеле. В предметной области
-// выделены два понятия: Order — заказ, который включает в себя даты бронирования
-// и контакты пользователя, и RoomAvailability — количество свободных номеров на
-// конкретный день.
-//
-// Задание:
-// - провести рефакторинг кода с выделением слоев и абстракций
-// - применить best-practices там где это имеет смысл
-// - исправить имеющиеся в реализации логические и технические ошибки и неточности
 package main
 
 import (
@@ -35,7 +26,7 @@ func main() {
 	r.Post("/orders", orderHandler.CreateOrder)
 
 	utils.LogInfo("Server listening on localhost:8080")
-	err := http.ListenAndServe(":8081", r)
+	err := http.ListenAndServe(":8080", r)
 
 	if errors.Is(err, http.ErrServerClosed) {
 		utils.LogInfo("Server closed")
